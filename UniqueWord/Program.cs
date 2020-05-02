6fda6b5bac9c5c3e2249352d167cca91f88dcee7
naiyapatel29 <naiyapatel29gmail.com>
2020-05-02 16:10:33 -0400

    added unit test

--- UniqueWord/Program.cs
+++ UniqueWord/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace UniqueWord
 {
@@ -6,7 +8,92 @@ namespace UniqueWord
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /**
+
+                Notes:
+
+                There is not enough information about the scope what kind of string and which special characters should be excluded.
+
+               I have developed the program based on following assumptions
+
+               1. All the special charaters "?,."" are not considered words.
+
+               2. All the words are converted to lowecase and compared so casesensitive words are not considered unique.
+
+               3. Unit Test covers the scenarion on how to handle empty string, string with all special charaters and a regular string to count number of unique words.
+
+    
+
+               I have used Dictionary with word as key and integer count as value.
+
+                * **/
+            string paragraph = "";
+            while (paragraph != "1")
+            {
+                Console.WriteLine("Enter Paragrah");
+
+                paragraph = Console.ReadLine();
+
+                UniqueWords uniqueWord = new UniqueWords();
+
+                //string paragraph = "Share your thoughts on something that inspired you to do more and go above and beyond to find opportunities to contribute to success of the organization \"you\" are working for? Share your thoughts on something that inspired you to do more and go above and beyond to find opportunities to contribute to success of    the organization you are working for.";
+
+                Dictionary<string, int> wordCount = uniqueWord.getUniqueWordCount(paragraph);
+
+                foreach (KeyValuePair<string, int> item in wordCount)
+
+                {
+
+                    Console.WriteLine(item.Key + "  " + item.Value);
+
+                }
+                Console.WriteLine("Press enter to continue and 1 to exit");
+
+                paragraph = Console.ReadLine();
+            }
+            System.Environment.Exit(0);
         }
+
     }
+
+    public class UniqueWords
+
+    {
+
+        public Dictionary<string, int> getUniqueWordCount(string paragraph)
+
+        {
+
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+            foreach (string word in Regex.Split(paragraph, @"["".,;?\s]+"))
+
+            {
+
+                if (dictionary.ContainsKey(word.ToLower()))
+
+                {
+
+                    dictionary[word.ToLower()] += 1;
+
+                }
+
+                else
+
+                {
+
+                    if (word.Trim() != "")
+
+                    { dictionary.Add(word.ToLower(), 1); }
+
+                }
+
+            }
+
+            return dictionary;
+
+        }
+
+    }
+
 }
---
+++ UniqueWordTest/UnitTest1.cs
@@ -0,0 +1,112 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+using UniqueWord;
+
+namespace UniqueWordTest
+{
+    public class Tests
+    {
+        UniqueWords uniqueWord = new UniqueWords();
+
+
+
+        [Test]
+
+        public void checkgetUniqueWordCount()
+
+        {
+
+            //Assert
+
+            var type = uniqueWord.GetType();
+            //Verify
+
+            Assert.IsNotNull(type.GetMethod("getUniqueWordCount"));
+
+        }
+
+        [Test]
+
+        public void ChekUniqueWordsArenotCaseSesitive()
+
+        {
+
+            //Assert
+
+            string paragraph = "Test test TesT TEST TEsT";
+
+            //Act
+
+            Dictionary<string, int> dictionary = uniqueWord.getUniqueWordCount(paragraph);
+
+            //Verify
+
+            Assert.AreEqual(dictionary.Count, 1);
+
+        }
+
+
+
+        [Test]
+
+        public void CheckPunctuationsAreNotConsideredAsWords()
+
+        {
+
+            //Assert
+
+            string paragraph = ".. ??    . ; ; ";
+
+            //Act
+
+            Dictionary<string, int> dictionary = uniqueWord.getUniqueWordCount(paragraph);
+
+            //Verify
+
+            Assert.AreEqual(dictionary.Count, 0);
+
+        }
+
+        [Test]
+
+        public void CheckUniquewordCountMethodHandlesEmptyString()
+
+        {
+
+            //Assert
+
+            string paragraph = "         ";
+
+            //Act
+
+            Dictionary<string, int> dictionary = uniqueWord.getUniqueWordCount(paragraph);
+
+            //Verify
+
+            Assert.AreEqual(dictionary.Count, 0);
+
+        }
+
+
+
+        public void CheckUniqueWordCountofGivenStringReturnCorrectly()
+
+        {
+
+            //Assert
+
+            string paragraph = "Share your thoughts on something that inspired you to do more and go above and beyond to find opportunities to contribute to success of the organization \"you\" are working for";
+
+            //Act
+
+            Dictionary<string, int> dictionary = uniqueWord.getUniqueWordCount(paragraph);
+
+            //Verify to words appears 4 times in the paragraph given above
+
+            Assert.AreEqual(dictionary["to"], 4);
+
+        }
+
+    }
+
+}
